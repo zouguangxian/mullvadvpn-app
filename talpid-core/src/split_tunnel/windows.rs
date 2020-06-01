6d0496ad469fd28dade557255b44046d53b0b215
David Lönnhager <david.l@mullvad.net>
2020-06-01 11:04:45 +0200

    Remove unneeded methods from SplitTunnel

--- talpid-core/src/split_tunnel/windows.rs
+++ talpid-core/src/split_tunnel/windows.rs
@@ -18,11 +18,6 @@ impl SplitTunnel {
         Ok(SplitTunnel { paths: vec![] })
     }
 
-    /// Get list of applications to exclude from the tunnel.
-    pub fn get_paths(&mut self) -> &[String] {
-        &self.paths
-    }
-
     /// Set a list of applications to exclude from the tunnel.
     pub fn set_paths<T: AsRef<str>>(&mut self, paths: &[T]) -> Result<(), Error> {
         self.paths.clear();
@@ -30,23 +25,6 @@ impl SplitTunnel {
             self.paths.push(path.as_ref().to_string());
         }
 
-        // WinExclude_SetAppPaths
-        Ok(())
-    }
-
-    /// Add a single application to exclude from the tunnel.
-    pub fn add_path(&mut self, new_path: &str) -> Result<(), Error> {
-        if !self.paths.iter().any(|path| path == new_path) {
-            self.paths.push(new_path.to_string());
-
-            // WinExclude_SetAppPaths
-        }
-        Ok(())
-    }
-
-    /// Stop forcing traffic to go outside the tunnel for a given app.
-    pub fn remove_path(&mut self, path: &str) -> Result<(), Error> {
-        self.paths.retain(|curpath| curpath != path);
         // WinExclude_SetAppPaths
         Ok(())
     }
