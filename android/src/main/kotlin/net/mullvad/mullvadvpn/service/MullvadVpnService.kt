0b2b571691d8c427f3d460ef7b0e09a9124ea823
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2020-07-04 03:06:03 +0000

    Store split tunnelling state in shared prefs.

--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/MullvadVpnService.kt
@@ -248,7 +248,7 @@ class MullvadVpnService : TalpidVpnService() {
             pendingAction = null
         }
 
-        val splitTunnelling = SplitTunnelling(connectionProxy).apply {
+        val splitTunnelling = SplitTunnelling(this, connectionProxy).apply {
             onChange = { excludedApps -> disallowedApps = excludedApps }
         }
 
--- android/src/main/kotlin/net/mullvad/mullvadvpn/service/SplitTunnelling.kt
+++ android/src/main/kotlin/net/mullvad/mullvadvpn/service/SplitTunnelling.kt
@@ -1,11 +1,16 @@
 package net.mullvad.mullvadvpn.service
 
+import android.content.Context
 import kotlin.properties.Delegates.observable
 import net.mullvad.mullvadvpn.model.TunnelState
 import net.mullvad.talpid.tunnel.ActionAfterDisconnect
 
-class SplitTunnelling(private val connectionProxy: ConnectionProxy) {
+private const val SHARED_PREFERENCES = "split_tunnelling"
+private const val KEY_ENABLED = "enabled"
+
+class SplitTunnelling(private val context: Context, private val connectionProxy: ConnectionProxy) {
     private val excludedApps = HashSet<String>()
+    private val preferences = context.getSharedPreferences(SHARED_PREFERENCES, Context.MODE_PRIVATE)
 
     val excludedAppList
         get() = if (enabled) {
@@ -14,7 +19,10 @@ class SplitTunnelling(private val connectionProxy: ConnectionProxy) {
             emptyList()
         }
 
-    var enabled by observable(false) { _, _, _ -> update() }
+    var enabled by observable(preferences.getBoolean(KEY_ENABLED, false)) { _, _, _ ->
+        enabledChanged()
+    }
+
     var onChange: ((List<String>) -> Unit)? = null
 
     fun isAppExcluded(appPackageName: String) = excludedApps.contains(appPackageName)
@@ -26,6 +34,15 @@ class SplitTunnelling(private val connectionProxy: ConnectionProxy) {
 
     fun includeApp(appPackageName: String) {
         excludedApps.remove(appPackageName)
+        update()
+    }
+
+    private fun enabledChanged() {
+        preferences.edit().apply {
+            putBoolean(KEY_ENABLED, enabled)
+            commit()
+        }
+
         update()
     }
 
